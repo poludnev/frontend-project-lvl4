ce87f64fc62e4125f172d7be88324cdea68bfcb7
poludnev <poludnev.roman@gmail.com>
2021-11-10 18:19:11 +0300

    step 10

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,9 +2,6 @@ import React, { useContext, useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
 import UserContext from '../contexts/userContext';
 import { useDispatch } from 'react-redux';
-// import { useHistory } from 'react-router-dom';
-// import { Formik, Form, Field } from 'formik';
-// import { io } from 'socket.io-client';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 
@@ -14,28 +11,20 @@ import Channels from './chat/Channels';
 import Messages from './chat/Messages';
 
 const Chat = () => {
-  // console.log('HOMEPAGEE INITIALISED');
-  const { user, logIn, logOut, AuthHeader } = useContext(UserContext);
+  const { logOut, AuthHeader } = useContext(UserContext);
   const [isLoading, setLoading] = useState(true);
 
-  // const history = useHistory();
   const dispatch = useDispatch();
   const { t } = useTranslation();
-  // const header = AuthHeader();
 
   useEffect(() => {
     const fetchContent = async () => {
-      // console.log('fetch content started');
-      //     // console.log('!!!!!headers:', header);
       try {
-        //       // console.log('!!!!!headers:', header);
         const header = AuthHeader();
         const { data } = await axios.get('/api/v1/data', { headers: { ...header } });
         dispatch(upLoadChannels(data.channels));
         dispatch(upLoadMessages(data.messages));
         setLoading(false);
-
-        // console.log('set loading', setLoading(false));
       } catch (error) {
         if (error.isAxiosError && error.response.status === 401) {
           logOut();
