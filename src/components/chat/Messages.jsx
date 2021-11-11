361cf4db2488fcebf7ba8b6f0d5ac6ffa530e39a
poludnev <poludnev.roman@gmail.com>
2021-11-11 15:33:01 +0300

    step 10

--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -35,13 +35,13 @@ const Messages = () => {
   });
 
   const renderMessage = ({ id, username, text }) => (
-      <div key={id} className="text-break mb-2">
+    <div key={id} className="text-break mb-2">
       <b>
       {username}
       </b>
       &nbsp;:&nbsp;
       {text}
-      </div>
+    </div>
   );
 
   return (
@@ -112,7 +112,8 @@ const Messages = () => {
                     >
                       <path
                         fillRule="evenodd"
-                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
+                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
+                      />
                     </svg>
                     <span className="visually-hidden">{t('messages.submitButtonLabel')}</span>
                   </Button>
--- src/slices/channelsSlice.js
+++ src/slices/channelsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import _ from 'lodash';
 
 const initialState = {
   channelsData: [],
@@ -10,25 +11,30 @@ export const channelsSlice = createSlice({
   initialState,
   reducers: {
     upLoadChannels: (state, action) => {
-      state.channelsData = [...action.payload];
+      // state.channelsData = [...action.payload];
+      state.channelsData.splice(0, Infinity, ...action.payload);
     },
     setCurrentChannel: (state, action) => {
-      state.currentChannelID = action.payload;
+      // state.currentChannelID = action.payload;
+      _.set(state, 'currentChannelID', action.payload);
     },
     addChannel: (state, action) => {
       state.channelsData.push(action.payload);
-      state.currentChannelID = action.payload.id;
+      _.set(state, 'currentChannelID', action.payload);
     },
     deleteChannel: (state, action) => {
       if (action.payload === state.currentChannelID) {
-        state.currentChannelID = initialState.currentChannelID;
+        // state.currentChannelID = initialState.currentChannelID;
+        _.set(state, 'currentChannelID', initialState.currentChannelID);
       }
       const filteredChannels = state.channelsData.filter((ch) => ch.id !== action.payload);
-      state.channelsData = filteredChannels;
+      // state.channelsData = filteredChannels;
+      state.channelsData.splice(0, Infinity, state.channelsData = filteredChannels);
     },
     changeNameChannel: (state, action) => {
       const channel = state.channelsData.find((ch) => ch.id === action.payload.id);
-      channel.name = action.payload.name;
+      // channel.name = action.payload.name;
+      _.set(channel, 'name', action.payload.name);
     },
   },
 });
