b52ba83cf15fa360bf71ea220bc969a25d7aed83
poludnev <poludnev.roman@gmail.com>
2021-11-15 13:05:27 +0300

    prefinal coomit

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -6,7 +6,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
-  entry: './src/indexLH.js',
   mode,
   resolve: {
     extensions: ['.js', '.jsx'],
