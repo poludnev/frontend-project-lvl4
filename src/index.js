e6c47bead8bfa7eb380e25c3bb731475d056096f
poludnev <poludnev.roman@gmail.com>
2021-11-22 16:43:44 +0300

    test

--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
+// // const app = async (socketClient) => {
 // const app = async (socketClient) => {
+//   // console.log('test');
+//   // const socketClient = io();
+//   const vdom = await init(socketClient);
+//   ReactDOM.render(vdom, document.querySelector('#chat'));
+//   return vdom;
+
+//   // return init(socketClient);
+// };
+
+// // const socket = io();
+
+// // app(socket);
+
 const app = async (socketClient) => {
-  // console.log('test');
-  // const socketClient = io();
   const vdom = await init(socketClient);
-  ReactDOM.render(vdom, document.querySelector('#chat'));
   return vdom;
-
-  // return init(socketClient);
 };
-
-const socket = io();
-
-app(socket);
 
 export default app;
