53df8e3cea043a5d45dcb19aa250933af3175ab8
poludnev <poludnev.roman@gmail.com>
2021-11-28 18:22:55 +0300

    Check2 fixing Moved toaster styling, add hidden span to channlecontrol button

--- src/components/chat/Channels.jsx
+++ src/components/chat/Channels.jsx
@@ -72,6 +72,7 @@ const Channels = () => {
                   variant={id === currentChannelID ? 'secondary' : 'light'}
                   className="flex-grow-0 dropdown-toggle dropdown-toggle-split btn"
                   aria-label="Управление каналом"
+                  title=""
                 >{'Управление каналом'}</Dropdown.Toggle>
                 <Dropdown.Menu>
                   <Dropdown.Item onClick={removeChannelHandler(id)} aria-label="Удалить">
