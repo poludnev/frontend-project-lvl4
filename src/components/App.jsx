8257f4e9f837fe00bd41001de591f67444557ac7
poludnev <poludnev.roman@gmail.com>
2021-11-28 18:46:54 +0300

    Check2 fixing Moved toaster styling, add hidden span to channlecontrol button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
 
   return (
     <Router>
-      <div className="d-flex flex-column h-100" aria-hidden={isShown}>
+      <div className="d-flex flex-column h-100" aria-hidden={false}>
         <ToastContainer />
         <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
           <div className="container">
